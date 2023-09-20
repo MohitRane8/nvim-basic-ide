043af0255261c0291a957f028dc7afc21efffe35
Mohit Rane <mohit.rane@colorado.edu>
2023-09-20 10:25:07 +0530

    Removed alpha.nvim and which-key.nvim and java formatting in null-ls

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ require "Lazy"
 require "autocommands"
 require "vimscripts"
 
+-- remove warnings from :checkhealth
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0

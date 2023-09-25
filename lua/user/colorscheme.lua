f116a729c4fd909a00550f32b50c8f0396b25532
Mohit Rane <mohit.rane@colorado.edu>
2023-09-25 15:21:28 +0530

    Added catppuccin colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,19 +1,26 @@
 local M = {
---  {
---    "folke/tokyonight.nvim",
---    commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
---  },
---  {
+  -- {
     "loctvl842/monokai-pro.nvim",
     commit = "e7b38bdb95fc144456703bb0ff5692c68093cc8e",
---  },
---  {
---    "Mofiqul/vscode.nvim",
---    commit = "dc5f37603ada51db13bc4009cbee628c5857a274",
---  },
-
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
+  --   lazy = true,
+  -- },
+  -- {
+  --   "Mofiqul/vscode.nvim",
+  --   commit = "dc5f37603ada51db13bc4009cbee628c5857a274",
+  --   lazy = true,
+  -- },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   commit = "b573866ef64122119668f375ebb794bcb3b518e2",    -- v1.4.0
+  --   lazy = true,
+  -- },
 }
 
 M.name = "monokai-pro"
@@ -21,18 +28,15 @@ M.name = "monokai-pro"
 function M.config()
 
   -- monokai-pro
---  if M.name == "monokai-pro" then
   local monokai = require "monokai-pro"
   monokai.setup ({
-    filter = "ristretto", -- classic | octagon | pro | machine | ristretto | spectrum
+    filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
   })
   monokai.load()
 
   -- vscode
---  elseif M.name == "vscode" then
---    local vscode = require "vscode"
---    vscode.load('dark')       -- light
---  end
+  -- local vscode = require "vscode"
+  -- vscode.load('dark')       -- light
 
   -- load colorscheme
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
